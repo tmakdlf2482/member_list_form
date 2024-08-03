a89171bc4b76b22662b0560c4513ab824ebe4643
dobby <tmakdlf2482@naver.com>
2024-08-03 15:47:59 +0900

    edit member add code

--- client/member_list_form.js
+++ client/member_list_form.js
@@ -1,7 +1,87 @@
 const member_form = document.querySelector('#member_form');
 const memListTbl = document.querySelector('.memListTbl');
-let pkNum = 1044;
-let imageCnt = 2;
+const member_del_btn = document.querySelector('#member_del_btn');
+
+function tblDrawList() {
+  axios.post('http://localhost:3000/list')
+  .then(res => {
+    let pkNum = 1043;
+    let imageCnt = 1;
+    
+    memListTbl.innerHTML = "";
+
+    res.data.forEach((member) => {
+      memListTbl.innerHTML +=
+        `
+          <table class="memListTbl">
+            <tr>
+              <th>
+                <input type="checkbox" name="" id="">
+              </th>
+              <th>번호</th>
+              <th>사진</th>
+              <th>사원명</th>
+              <th>부서</th>
+              <th>직급</th>
+              <th>수정</th>
+              <th>삭제</th>
+            </tr>
+            <tr>
+              <td rowspan="2"><input type="checkbox" name="" id=""></td>
+              <td rowspan="2">${pkNum++}</td>
+              <td>
+                <img src="" alt="사원${imageCnt++}의 사진">
+              </td>
+              <td>${member.name}</td>
+              <td>${member.dept}</td>
+              <td>${member.rank}</td>
+              <td rowspan="2"><button>Edit</button></td>
+              <td rowspan="2"><button>Delete</button></td>
+            </tr>
+            <tr>
+              <td colspan="4">
+                <table class="subTbl" width="100%" cellspacing="0">
+                  <tr>
+                    <td width="10px">번호</td>
+                    <td>댓글내용</td>
+                    <td>작성자</td>
+                    <td>수정</td>
+                    <td>삭제</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>멋진 사진입니다.</td>
+                    <td>박문수</td>
+                    <td>수정</td>
+                    <td>삭제</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>좋아요^^ 퍼가요~</td>
+                    <td>일지매</td>
+                    <td>수정</td>
+                    <td>삭제</td>
+                  </tr>
+                  <tr>
+                    <td colspan="5">
+                      댓글
+                      <input type="text">
+                      작성자
+                      <input type="text">
+                      <button>댓글입력</button>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        `;
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
 
 member_form.addEventListener('submit', (e) => {
   e.preventDefault(); // 기본 폼 제출 동작 방지
@@ -9,69 +89,23 @@ member_form.addEventListener('submit', (e) => {
   let name = document.querySelector('#name').value;
   let dept = document.querySelector('#dept').value;
   let rank = document.querySelector('#rank').value;
-  
-  axios.post('http://localhost:3000/member_input_form', {
+
+  axios.post('http://localhost:3000/submit', {
     name: name,
     dept: dept,
     rank: rank,
   })
-  .then(res => {
-    console.log("서버 응답 : ", res.data);
-    member_form.reset(); // 이름, 부서, 직급 입력폼 초기화
+    .then(res => {
+      console.log("서버 응답 : ", res.data);
+      member_form.reset(); // 이름, 부서, 직급 입력폼 초기화
 
-    memListTbl.innerHTML +=
-    `
-      <tr>
-        <td rowspan="2"><input type="checkbox" name="" id=""></td>
-        <td rowspan="2">${pkNum++}</td>
-        <td>
-          <img src="" alt="사원${imageCnt++}의 사진">
-        </td>
-        <td>${res.data[res.data.length - 1].name}</td>
-        <td>${res.data[res.data.length - 1].dept}</td>
-        <td>${res.data[res.data.length - 1].rank}</td>
-        <td rowspan="2"><button>Edit</button></td>
-        <td rowspan="2"><button>Delete</button></td>
-      </tr>
-      <tr>
-        <td colspan="4">
-          <table class="subTbl" width="100%" cellspacing="0">
-            <tr>
-              <td width="10px">번호</td>
-              <td>댓글내용</td>
-              <td>작성자</td>
-              <td>수정</td>
-              <td>삭제</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>멋진 사진입니다.</td>
-              <td>박문수</td>
-              <td>수정</td>
-              <td>삭제</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>좋아요^^ 퍼가요~</td>
-              <td>일지매</td>
-              <td>수정</td>
-              <td>삭제</td>
-            </tr>
-            <tr>
-              <td colspan="5">
-                댓글
-                <input type="text">
-                작성자
-                <input type="text">
-                <button>댓글입력</button>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-    `
-  })
-  .catch(err => {
-    console.log("서버 오류 : ", err);
-  });
+      tblDrawList();
+    })
+    .catch(err => {
+      console.log("서버 오류 : ", err);
+    });
+});
+
+member_del_btn.addEventListener('click', () => {
+
 });
--- server/index.js
+++ server/index.js
@@ -9,18 +9,24 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const memberList = [];
+const memberList = [
+  {name: '홍길동', dept: '개발', rank: '대리'},
+];
 
-app.post('/member_input_form', (req, res) => {
+const server = http.createServer(app);
+server.listen(app.get('port'), () => {
+  console.log(`connecting nodejs server >>> http://localhost:${app.get('port')}`);
+});
+
+app.post('/list', (req, res) => {
+  res.status(200).json(memberList);
+});
+
+app.post('/submit', (req, res) => {
   console.log("이름 :", req.body.name, "/ 부서 :", req.body.dept, "/ 직급 :", req.body.rank);
 
   memberList.push( {name: req.body.name, dept: req.body.dept, rank: req.body.rank} );
 
   // 클라이언트에게 응답
-  res.status(200).json(memberList);
-});
-
-const server = http.createServer(app);
-server.listen(app.get('port'), () => {
-  console.log(`connecting nodejs server >>> http://localhost:${app.get('port')}`);
+  res.status(200).json({success: true});
 });
