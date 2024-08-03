1a4b77b2035a2b09875411270767bd2380ab5494
dobby <tmakdlf2482@naver.com>
2024-08-03 20:12:42 +0900

    member delete

--- client/member_list_form.js
+++ client/member_list_form.js
@@ -1,81 +1,83 @@
 const member_form = document.querySelector('#member_form');
 const memListTbl = document.querySelector('.memListTbl');
-const member_del_btn = document.querySelector('#member_del_btn');
 
 function tblDrawList() {
   axios.post('http://localhost:3000/list')
   .then(res => {
-    let pkNum = 1043;
+    let pkNum = 1;
     let imageCnt = 1;
     
     memListTbl.innerHTML = "";
 
     res.data.forEach((member) => {
       memListTbl.innerHTML +=
-        `
-          <table class="memListTbl">
-            <tr>
-              <th>
-                <input type="checkbox" name="" id="">
-              </th>
-              <th>번호</th>
-              <th>사진</th>
-              <th>사원명</th>
-              <th>부서</th>
-              <th>직급</th>
-              <th>수정</th>
-              <th>삭제</th>
-            </tr>
-            <tr>
-              <td rowspan="2"><input type="checkbox" name="" id=""></td>
-              <td rowspan="2">${pkNum++}</td>
-              <td>
-                <img src="" alt="사원${imageCnt++}의 사진">
-              </td>
-              <td>${member.name}</td>
-              <td>${member.dept}</td>
-              <td>${member.rank}</td>
-              <td rowspan="2"><button>Edit</button></td>
-              <td rowspan="2"><button>Delete</button></td>
-            </tr>
-            <tr>
-              <td colspan="4">
-                <table class="subTbl" width="100%" cellspacing="0">
-                  <tr>
-                    <td width="10px">번호</td>
-                    <td>댓글내용</td>
-                    <td>작성자</td>
-                    <td>수정</td>
-                    <td>삭제</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>멋진 사진입니다.</td>
-                    <td>박문수</td>
-                    <td>수정</td>
-                    <td>삭제</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>좋아요^^ 퍼가요~</td>
-                    <td>일지매</td>
-                    <td>수정</td>
-                    <td>삭제</td>
-                  </tr>
-                  <tr>
-                    <td colspan="5">
-                      댓글
-                      <input type="text">
-                      작성자
-                      <input type="text">
-                      <button>댓글입력</button>
-                    </td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </table>
-        `;
+      `
+        <table class="memListTbl">
+          <tr>
+            <th>
+              <input type="checkbox" name="" id="">
+            </th>
+            <th>번호</th>
+            <th>사진</th>
+            <th>사원명</th>
+            <th>부서</th>
+            <th>직급</th>
+            <th>수정</th>
+            <th>삭제</th>
+          </tr>
+          <tr data-seq="${pkNum}">
+            <td rowspan="2"><input type="checkbox" name="" id=""></td>
+            <td rowspan="2">${member.pkNum}</td>
+            <td>
+              <img src="" alt="회원${imageCnt}의 사진">
+            </td>
+            <td>${member.name}</td>
+            <td>${member.dept}</td>
+            <td>${member.rank}</td>
+            <td rowspan="2"><button>Edit</button></td>
+            <td rowspan="2"><button onClick="handleDelBtn(this)">Delete</button></td>
+          </tr>
+          <tr>
+            <td colspan="4">
+              <table class="subTbl" width="100%" cellspacing="0">
+                <tr>
+                  <td width="10px">번호</td>
+                  <td>댓글내용</td>
+                  <td>작성자</td>
+                  <td>수정</td>
+                  <td>삭제</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>멋진 사진입니다.</td>
+                  <td>박문수</td>
+                  <td>수정</td>
+                  <td>삭제</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>좋아요^^ 퍼가요~</td>
+                  <td>일지매</td>
+                  <td>수정</td>
+                  <td>삭제</td>
+                </tr>
+                <tr>
+                  <td colspan="5">
+                    댓글
+                    <input type="text">
+                    작성자
+                    <input type="text">
+                    <button>댓글입력</button>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      `;
+
+      pkNum++;
+      imageCnt++;
     });
   })
   .catch(err => {
@@ -95,17 +97,27 @@ member_form.addEventListener('submit', (e) => {
     dept: dept,
     rank: rank,
   })
-    .then(res => {
-      console.log("서버 응답 : ", res.data);
-      member_form.reset(); // 이름, 부서, 직급 입력폼 초기화
+  .then(res => {
+    // console.log("서버 응답 : ", res.data);
+    member_form.reset(); // 이름, 부서, 직급 입력폼 초기화
 
-      tblDrawList();
-    })
-    .catch(err => {
-      console.log("서버 오류 : ", err);
-    });
+    tblDrawList();
+  })
+  .catch(err => {
+    console.log("서버 오류 : ", err);
+  });
 });
 
-member_del_btn.addEventListener('click', () => {
-
-});
+function handleDelBtn(delBtn) {
+  const trElement = delBtn.parentElement.parentElement;
+  const pkNum = trElement.dataset.seq;
+  
+  axios.post('http://localhost:3000/delete', {pkNum: pkNum})
+  .then((res) => {
+    console.log("남은 멤버 리스트 : ", res.data);
+    tblDrawList();
+  })
+  .catch(err => {
+    console.log("서버 오류 : ", err);
+  });
+}
--- server/index.js
+++ server/index.js
@@ -9,9 +9,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const memberList = [
-  {name: '홍길동', dept: '개발', rank: '대리'},
-];
+const memberList = [];
+let pkNum = 1;
 
 const server = http.createServer(app);
 server.listen(app.get('port'), () => {
@@ -19,14 +18,30 @@ server.listen(app.get('port'), () => {
 });
 
 app.post('/list', (req, res) => {
+  console.log("남은 회원 리스트 :", memberList);
+
   res.status(200).json(memberList);
 });
 
 app.post('/submit', (req, res) => {
-  console.log("이름 :", req.body.name, "/ 부서 :", req.body.dept, "/ 직급 :", req.body.rank);
+  console.log("회원가입한 회원 /// 이름 :", req.body.name, "/ 부서 :", req.body.dept, "/ 직급 :", req.body.rank);
 
-  memberList.push( {name: req.body.name, dept: req.body.dept, rank: req.body.rank} );
+  memberList.push( {pkNum: pkNum++, name: req.body.name, dept: req.body.dept, rank: req.body.rank} );
 
   // 클라이언트에게 응답
   res.status(200).json({success: true});
+});
+
+app.post('/delete', (req, res) => {
+  let pkNum = parseInt(req.body.pkNum); // string -> number
+  
+  let idx = memberList.findIndex((ind) => {
+    return ind.pkNum == pkNum;
+  });
+
+  if (idx != -1) {
+    memberList.splice(idx, 1);
+  }
+  
+  res.status(200).json(memberList);
 });
